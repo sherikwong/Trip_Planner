0597f38b1282d9d2b3dfd21f352e33cec576be07
Sher iKwong <sherikwo@buffalo.edu>
2018-05-01 17:00:30 -0400

    bug checking

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!')
+const mapboxgl = require("mapbox-gl");
+
+console.log("Hello");
+
+mapboxgl.accessToken = "pk.eyJ1IjoibGFycG9sbGFjayIsImEiOiJjamdvNWtycXowMDdoMnhwbnFnZTN0c2ZpIn0.zAppLpXWr-Dx9FiaAhJ7tQ";
+// const map = new mapboxgl.Map({
+//   container: "map",
+//   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+//   zoom: 12, // starting zoom
+//   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+// });
+
+window.out = mapboxgl;
+const map = new mapboxgl.Map({
+  container: "map"
+});
